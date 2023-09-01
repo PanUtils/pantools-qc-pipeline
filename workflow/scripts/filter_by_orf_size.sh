489061235f37568f48a1fc68fa111d81482e8596
robin <roel.vanesch@wur.nl>
2023-09-01 14:41:26 +0200

    replaced gff with gff3 in filter_by_orf_size.sh

--- workflow/scripts/filter_by_orf_size.sh
+++ workflow/scripts/filter_by_orf_size.sh
@@ -2,12 +2,12 @@
 # Robin van Esch - 05-12-2022
 
 orf_size="${snakemake_wildcards[orf_size]}"
-output="${snakemake_output[0]/%_sup=$orf_size.gff/.gff}"
+output="${snakemake_output[0]/%_sup=$orf_size.gff3/.gff3}"
 
 agat_sp_filter_by_ORF_size.pl \
   --size "$orf_size" \
   --test '>=' --gff "${snakemake_input}" \
   --output "$output" > /dev/null
 
-echo "$(basename "$output" .gff).agat.log"
-mv "$(basename "$output" .gff)_longest_isoform.agat.log" "${snakemake_log}"
+echo "$(basename "$output" .gff3).agat.log"
+mv "$(basename "$output" .gff3)_longest_isoform.agat.log" "${snakemake_log}"
